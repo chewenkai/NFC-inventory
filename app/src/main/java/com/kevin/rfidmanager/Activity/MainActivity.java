6ed1b0409e6c528057f312b6df258f89f5c29fcb
KevinChe <chewenkaich@gmail.com>
2017-02-04 22:20:38 +0800

    #36 37

--- app/src/main/java/com/kevin/rfidmanager/Activity/MainActivity.java
+++ app/src/main/java/com/kevin/rfidmanager/Activity/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
                         adapter.tab3=null;
                         adapter.tab4=null;
                         adapter = null;
+                        ((MyApplication) getApplication()).setCurrentItemID(ConstantManager.DEFAULT_RFID);
                         finish();
                         break;
                     default:
